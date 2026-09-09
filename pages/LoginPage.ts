aca3ca20e3382631ad5b7991421e59a23cbc78eb
Paramjot Singh <singhparamjot6789@gmail.com>
2026-09-10 00:02:22 +0530

    learned POM

---
+++ pages/LoginPage.ts
@@ -0,0 +1,23 @@
+import { Page, expect } from '@playwright/test';
+
+export class LoginPage {
+  constructor(private readonly page: Page) {}
+
+  async goto() {
+    await this.page.goto('https://www.saucedemo.com/');
+  }
+
+  async login(username: string, password: string) {
+    await this.page.locator('[data-test="username"]').fill(username);
+    await this.page.locator('[data-test="password"]').fill(password);
+    await this.page.locator('[data-test="login-button"]').click();
+  }
+
+  async expectErrorVisible() {
+    await expect(this.page.locator('[data-test="error"]')).toBeVisible();
+  }
+
+  async expectErrorText(text: string) {
+    await expect(this.page.getByText(text)).toBeVisible();
+  }
+}
--- tests/saucedemo.spec.ts
+++ tests/saucedemo.spec.ts
@@ -1,45 +1,17 @@
-import { test, expect } from '@playwright/test';
+import {test, expect} from '@playwright/test';
+import { LoginPage } from '../pages/LoginPage';
 
-test('valid_login', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
-  await page.locator('[data-test="username"]').fill('standard_user');
-  await page.locator('[data-test="password"]').fill('secret_sauce');
-  await page.locator('[data-test="login-button"]').click();
+test('valid_login', async ({page}) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.goto();
+  await loginPage.login('standard_user', 'secret_sauce');
   await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
 });
 
-test('bad_pswd', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
-  await page.locator('[data-test="username"]').fill('standard_user');
-  await page.locator('[data-test="password"]').fill('abcd');
-  await page.locator('[data-test="login-button"]').click();
-  await expect(page.locator('[data-test="error"]')).toBeVisible();
-  await expect(page.getByText('Epic sadface: Username and password do not match any user in this service')).toBeVisible();
+test('bad_pswd', async ({page}) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.goto();
+  await loginPage.login('standard_user', 'abcd');
+  await loginPage.expectErrorVisible();
+  await loginPage.expectErrorText('Epic sadface: Username and password do not match any user in this service');
 });
-
-
-// test('test', async ({ page }) => {
-//   await page.goto('https://www.saucedemo.com/');
-//   await page.locator('[data-test="username"]').click();
-//   await page.locator('[data-test="username"]').fill('standard_user');
-//   await page.locator('[data-test="username"]').press('Tab');
-//   await page.locator('[data-test="password"]').fill('secret_sauce');
-//   await page.locator('div').nth(4).click();
-//   await page.locator('[data-test="login-button"]').click();
-//   await page.locator('[data-test="add-to-cart-sauce-labs-onesie"]').click();
-//   await page.locator('[data-test="add-to-cart-test.allthethings()-t-shirt-(red)"]').click();
-//   await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
-//   await page.locator('[data-test="shopping-cart-link"]').click();
-//   await page.locator('[data-test="checkout"]').click();
-//   await page.locator('[data-test="firstName"]').click();
-//   await page.locator('[data-test="firstName"]').fill('test');
-//   await page.locator('[data-test="firstName"]').press('Tab');
-//   await page.locator('[data-test="lastName"]').fill('final');
-//   await page.locator('[data-test="lastName"]').press('Tab');
-//   await page.locator('[data-test="postalCode"]').fill('213');
-//   await page.locator('[data-test="continue"]').click();
-//   await page.locator('[data-test="finish"]').click();
-//   const downloadPromise = page.waitForEvent('download');
-//   await page.locator('[data-test="generate-pdf-order"]').click();
-//   const download = await downloadPromise;
-// });
