61ec435a6a1ba7aced5d6d7fb0bde70384a1d1c1
Paramjot Singh <singhparamjot6789@gmail.com>
2026-09-12 23:24:36 +0530

    automated an API test

---
+++ tests/api/posts.spec.ts
@@ -0,0 +1,31 @@
+import {test,expect}  from '@playwright/test';
+
+const url = "https://jsonplaceholder.typicode.com/posts";
+
+test('get /posts returns 200 and 100 items', async({request}) => {
+    const res = await request.get(url);
+   
+    expect(res.status()).toBe(200);
+
+    const body = await res.json();
+    expect(Array.isArray(body)).toBe(true);
+    expect(body.length).toBe(100);
+});
+
+test('POST /post creates a resource', async ({request}) => {
+    const res = await request.post(url, {
+        data: {
+            title  : 'sdet-api',
+            body: 'learning playwright request',
+            userId: 1,
+        },
+    });
+
+    expect(res.status()).toBe(201);
+
+    const body = await res.json();
+    expect(body.title).toBe('sdet-api');
+    expect(body).toHaveProperty('id');
+    
+}
+);
